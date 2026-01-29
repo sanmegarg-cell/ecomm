c53e7d7630212e48539bdfa06397736c56d765f4
ruchika gupta <rgupta@ruchikas-iMac.local>
2026-01-29 23:30:31 +0100

    workflow build

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,15 @@ import "./styles/index.css";
 import { Toaster } from "./app/components/ui/sonner";
 import React from "react";
   
-const container = document.getElementById('root') as HTMLElement;
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
