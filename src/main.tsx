a5837134bd285f18ac5862e8d4eb9b9e33fd90bf
ruchika gupta <rgupta@ruchikas-iMac.local>
2026-01-29 23:02:04 +0100

    change for npm

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 
-  import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-  import { Toaster } from "./app/components/ui/sonner";
-
-createRoot(document.getElementById("root")!).render(
+import App from "./app/App.tsx";
+import "./styles/index.css";
+import { Toaster } from "./app/components/ui/sonner";
+  
+const root = createRoot(document.getElementById('root'));
+root.render(
   <BrowserRouter>
     <App />
     <Toaster />
